d58e8544abf657a73ff4008ce841363f108a56cd
Nicole Essex <Essex@Nicole-Essexs-MacBook-Pro-2.local>
2018-02-21 11:16:55 -0700

    finished what I could pseudo coded the rest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,9 @@
-function stopTimer ( duration, display){
-    
-}
 
+var ansCrct = 0;
+var ansWrng = 0;
+// var timer = duration, minutes, seconds;
 
+//start timer function to be called when event listener on.click starts the game
 function startTimer (duration, display){
     var timer = duration, minutes, seconds;
     setInterval(function() {
@@ -16,28 +17,65 @@ function startTimer (duration, display){
 
         if (--timer < 0) {
             timer = duration;
-            alert("Better Luck Next Time")
+            alert("Better Luck Next Time" 
+            + " Answers Correct:" + ansCrct + 
+            " Answers Wrong:" + ansWrng);
         
         }
     }, 1000);
 }
 
 function startGame(){
-    $('#button').on('click', function (event) {
+    $('#play').on('click', function (event) {
         $("form").show();
-    var oneMinute = 60,
+    var oneMinute = 20,
         display = document.querySelector('#countDown');
     startTimer(oneMinute, display);
     })
 
 }   
 
+//When time is up store user answer selection in question variables. Compare checked radio with correct value. Increase correct answer variable or increase wong answer variable with if/else statements.
 function endGame() {
-    var q1answer = $('input[name="trivia-1"]:checked');
-    if(q1answer.val()==="ute"){
-        correct ++;
+    var q1answer = $('input[name="trivia-3"]:checked');
+    if(q1answer == "ute"){
+        ansCrct++;
+        console.log(ansCrct);
     }else {
-        incorrect ++;
+        ansWrng++;
     }
+    var q2answer = $('input[name="trivia-2"]:checked');
+    if(q2answer.val() == "cougar"){
+         console.log(q2answer.val());
+    }else {
+        ansWrng++;
+    }
+    var q3answer = $('input[name="trivia-3"]:checked');
+    if(q3answer.val() === "topaz"){
+        ansCrct++;  
+    }else {
+        ansWrng++;
+    }
+    var q4answer = $('input[name="trivia-4"]:checked');
+    if(q4answer.val() === "coal"){
+        ansCrct++;
+    }else {
+        ansWrng++;
+    }
+    var q5answer = $('input[name="trivia-5"]:checked');
+    if(q5answer.val() === "kings") {
+        ansCrct++;
+    } else {
+        ansWrng++;
+    }
+     
 }
 startGame();
+endGame();
+// restart game function
+    // reset ('form') to hidden;
+    // reset timer to 1:00;
+    // reset ansWrng varibale and ansCrct variable to 0;
+
+
+
