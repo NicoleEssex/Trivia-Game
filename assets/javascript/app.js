3dff3f1d9624bc82bb29281371c15f231580abe5
Nicole Essex <Essex@Nicole-Essexs-MacBook-Pro-2.local>
2018-05-23 14:09:00 -0600

    limited radio selection, cleaned up code, shortened time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,24 @@
 
 var ansCrct = 0;
 var ansWrng = 0;
-// var timer = duration, minutes, seconds;
+
+
 
 //start timer function to be called when event listener on.click starts the game
 function startTimer (duration, display){
-    var timer = duration, minutes, seconds;
+    var timer = duration, seconds;
     setInterval(function() {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+        // minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 30, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
+        // minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        display.textContent = seconds;
 
-        if (--timer < 0) {
+        if (--timer < -1) {
             timer = duration;
+
             alert("Better Luck Next Time" 
             + " Answers Correct:" + ansCrct + 
             " Answers Wrong:" + ansWrng);
@@ -28,54 +30,89 @@ function startTimer (duration, display){
 function startGame(){
     $('#play').on('click', function (event) {
         $("form").show();
-    var oneMinute = 60,
+    var oneMinute = 30,
         display = document.querySelector('#countDown');
     startTimer(oneMinute, display);
+    $(":radio[name=trivia]").change(function() {
+        var userGuess1 = (this.value);
+        console.log(userGuess1);
+        if(userGuess1 === 'ute') {
+            console.log('Correct');
+            ansCrct++;
+            console.log(ansCrct);
+        }else {
+            console.log('Wrong');
+            ansWrng++;
+            console.log(ansWrng);
+        }
+        $(":radio[name='trivia']:not(:checked)").attr("disabled", true);       
+    });
+    $(":radio[name=trivia1]").change(function() {
+        var userGuess2 = (this.value);
+        console.log(userGuess2);
+        if(userGuess2 === 'cougar') {
+            console.log('Correct');
+            ansCrct++;
+            console.log(ansCrct);
+        }else {
+            console.log('Wrong');
+            ansWrng++;
+            console.log(ansWrng);
+        }
+        $(":radio[name='trivia1']:not(:checked)").attr("disabled", true);       
+    });
+    $(":radio[name=trivia2]").change(function() {
+        var userGuess3 = (this.value);
+        console.log(userGuess3);
+        if(userGuess3 === 'topaz') {
+            console.log('Correct');
+            ansCrct++;
+            console.log(ansCrct);
+        }else {
+            console.log('Wrong');
+            ansWrng++;
+            console.log(ansWrng);
+        }
+        $(":radio[name='trivia2']:not(:checked)").attr("disabled", true); 
+    });
+    $(":radio[name=trivia3]").change(function() {
+        var userGuess4 = (this.value);
+        console.log(userGuess4);
+        if(userGuess4 === 'coal') {
+            console.log('Correct');
+            ansCrct++;
+            console.log(ansCrct);
+        }else {
+            console.log('Wrong');
+            ansWrng++;
+            console.log(ansWrng);
+        }
+        $(":radio[name='trivia3']:not(:checked)").attr("disabled", true); 
+    });
+    $(":radio[name=trivia4]").change(function() {
+        var userGuess5 = (this.value);
+        console.log(userGuess5);
+        if(userGuess5 === 'kings') {
+            console.log('Correct');
+            ansCrct++;
+            console.log(ansCrct);
+        }else {
+            console.log('Wrong');
+            ansWrng++;
+            console.log(ansWrng);
+        }
+        $(":radio[name='trivia4']:not(:checked)").attr("disabled", true); 
+    });
     })
 
-}   
-
-//When time is up store user answer selection in question variables. Compare checked radio with correct value. Increase correct answer variable or increase wong answer variable with if/else statements.
-function endGame() {
-    var q1answer = $('input[name="trivia-3"]:checked');
-    if(q1answer == "ute"){
-        ansCrct++;
-        console.log(ansCrct);
-    }else {
-        ansWrng++;
-    }
-    var q2answer = $('input[name="trivia-2"]:checked');
-    if(q2answer.val() == "cougar"){
-         console.log(q2answer.val());
-    }else {
-        ansWrng++;
-    }
-    var q3answer = $('input[name="trivia-3"]:checked');
-    if(q3answer.val() === "topaz"){
-        ansCrct++;  
-    }else {
-        ansWrng++;
-    }
-    var q4answer = $('input[name="trivia-4"]:checked');
-    if(q4answer.val() === "coal"){
-        ansCrct++;
-    }else {
-        ansWrng++;
-    }
-    var q5answer = $('input[name="trivia-5"]:checked');
-    if(q5answer.val() === "kings") {
-        ansCrct++;
-    } else {
-        ansWrng++;
-    }
-     
 }
-startGame();
-endGame();
+startGame();   
+//TODO
+//change alert to popup with option to play again.
 // restart game function
     // reset ('form') to hidden;
-    // reset timer to 1:00;
-    // reset ansWrng varibale and ansCrct variable to 0;
+    // reset timer to 30;
+    // reset ansWrng variable and ansCrct variable to 0;
 
 
 
